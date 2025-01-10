4992e6a7d49546b474e722c69852656da85fe8ea
Nick Mokhnatov <softilium@gmail.com>
2025-01-10 21:36:07 +0100

    devops rearranges

---
+++ devops/database-restore-local.cmd
@@ -0,0 +1,11 @@
+set PGPASSWORD=0000
+set PGPATH=D:\Program Files\PostgreSQL\16.4-5.1C\bin
+
+rar x rsdata.rar
+
+"%PGPATH%\psql.exe" -U postgres -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE datname = 'rsdata';"
+"%PGPATH%\psql.exe" -U postgres -c "drop database if exists rsdata"
+"%PGPATH%\psql.exe" -U postgres -c "create database rsdata"
+"%PGPATH%\psql.exe" --dbname=rsdata -U postgres < "backup.sql"
+
+delete backup.sql
