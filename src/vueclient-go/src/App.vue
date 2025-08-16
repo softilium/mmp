5755d308be829991b5f094cef1ac7e5d81cb60f4
Nick Mokhnatov <softilium@gmail.com>
2025-08-16 19:48:28 +0200

    svg icons for navbar

--- src/vueclient-go/src/App.vue
+++ src/vueclient-go/src/App.vue
@@ -52,16 +52,40 @@ const SendMsg = async () => {
       class="navbar fixed-top navbar-expand-sm navbar-toggleable-sm navbar-light bg-white border-bottom box-shadow.mb-3"
     >
       <div class="container-fluid">
-        <RouterLink class="btn btn-outline-secondary btn-sm" to="/"
-          ><img
-            src="/src/assets/bkg.png"
-            width="24px"
-            height="24px"
-          />&nbsp;Витрины</RouterLink
+        <RouterLink class="btn" to="/">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="32"
+            height="32"
+            fill="currentColor"
+            class="bi bi-house-heart-fill"
+            viewBox="0 0 16 16"
+          >
+            <path
+              d="M7.293 1.5a1 1 0 0 1 1.414 0L11 3.793V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v3.293l2.354 2.353a.5.5 0 0 1-.708.707L8 2.207 1.354 8.853a.5.5 0 1 1-.708-.707z"
+            />
+            <path
+              d="m14 9.293-6-6-6 6V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5zm-6-.811c1.664-1.673 5.825 1.254 0 5.018-5.825-3.764-1.664-6.691 0-5.018"
+            />
+          </svg> </RouterLink
         >&nbsp;&nbsp;
-        <RouterLink class="btn btn-outline-secondary btn-sm" to="/search"
-          >Поиск</RouterLink
-        >
+        <RouterLink class="btn" to="/search">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="32"
+            height="32"
+            fill="currentColor"
+            class="bi bi-search-heart"
+            viewBox="0 0 16 16"
+          >
+            <path
+              d="M6.5 4.482c1.664-1.673 5.825 1.254 0 5.018-5.825-3.764-1.664-6.69 0-5.018"
+            />
+            <path
+              d="M13 6.5a6.47 6.47 0 0 1-1.258 3.844q.06.044.115.098l3.85 3.85a1 1 0 0 1-1.414 1.415l-3.85-3.85a1 1 0 0 1-.1-.115h.002A6.5 6.5 0 1 1 13 6.5M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11"
+            />
+          </svg>
+        </RouterLink>
         <span v-if="!ctx.userInfo.id && ctx.isTg()" class="text-danger">
           Напишите в чат бота любое сообщение для авто-регистрации&nbsp;
         </span>
